4eabc1849113204f33e2f2f855596c98c70944fc
jakesimonds <jakesimonds@gmail.com>
2022-08-09 17:31:27 -0400

    More error tests

--- test/DriverTest.java
+++ test/DriverTest.java
@@ -63,7 +63,7 @@ public class DriverTest {
 
     }
     @Test(expected=IllegalArgumentException.class)
-    public void TestThorwErrorWhenItemsLessThan10(){
+    public void TestThrowErrorWhenItemsLessThan10(){
         FighterImpl testFighter = new FighterImpl("Joe",12,10);
         FighterImpl testFighter2 = new FighterImpl("Jane",10,10);
 
@@ -99,7 +99,7 @@ public class DriverTest {
     }
 
     @Test(expected=IllegalArgumentException.class)
-    public void TestThorwErrorWhenItemsMoreThan10(){
+    public void TestThrowErrorWhenItemsMoreThan10(){
         FighterImpl testFighter = new FighterImpl("Joe",12,10);
         FighterImpl testFighter2 = new FighterImpl("Jane",10,10);
 
@@ -153,6 +153,57 @@ public class DriverTest {
         FootWear footwear1 = new FootWear("Shoe","Blue",4,4);
         FootWear footwear2 = new FootWear("Sandal","Purple",1,6);
         FootWear footwear3 = new FootWear("Flip Flop","Black",3,5);
+        FootWear footwear4 = new FootWear("Roller Skate","Green",7,3);
+
+
+        //gears list
+        ArrayList<Gear> testGears = new ArrayList<Gear>();
+        testGears.add(headgear1);
+        testGears.add(headgear2);
+        testGears.add(handGear1);
+        testGears.add(handGear2);
+        testGears.add(handGear3);
+        testGears.add(handGear4);
+        testGears.add(footwear1);
+        testGears.add(footwear2);
+        //footwear 3 twice
+        testGears.add(footwear3);
+        testGears.add(footwear3);
+
+        Driver driver = new Driver(testFighter,testFighter2,testGears);
+
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void TestThrowErrorNullGears(){
+        FighterImpl testFighter = new FighterImpl("Joe",12,10);
+        FighterImpl testFighter2 = new FighterImpl("Jane",10,10);
+
+        //10 items
+        ArrayList<Gear> testGears = null;
+
+
+        Driver driver = new Driver(testFighter,testFighter2,testGears);
+
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void TestThrowErrorWhenFighterIsNull(){
+        FighterImpl testFighter = new FighterImpl("Joe",12,10);
+        FighterImpl testFighter2 = null;
+
+        //10 items
+        Gear headgear1 = new HeadGear("Hat","Large",5);
+        Gear headgear2 = new HeadGear("Helmet","Old",10);
+
+        HandGear handGear1 = new HandGear("Shield","Red",2,8);
+        HandGear handGear2 = new HandGear("Glove","Pink",4,6);
+        HandGear handGear3 = new HandGear("Ring","Yellow",7,5);
+        HandGear handGear4 = new HandGear("Fingerless Glove","Blue",3,3);
+
+        FootWear footwear1 = new FootWear("Shoe","Blue",4,4);
+        FootWear footwear2 = new FootWear("Sandal","Purple",1,6);
+        FootWear footwear3 = new FootWear("Flip Flop","Black",3,5);
         //FootWear footwear4 = new FootWear("Roller Skate","Green",7,3);
 
         //FootWear footwear5 = new FootWear("Roller Skate","Green",7,3);
@@ -174,5 +225,45 @@ public class DriverTest {
         Driver driver = new Driver(testFighter,testFighter2,testGears);
 
     }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void TestThrowErrorWhenFighterObjectCalledTwice_ex_FightsThemselves(){
+        FighterImpl testFighter = new FighterImpl("Joe",12,10);
+
+        //10 items
+        Gear headgear1 = new HeadGear("Hat","Large",5);
+        Gear headgear2 = new HeadGear("Helmet","Old",10);
+
+        HandGear handGear1 = new HandGear("Shield","Red",2,8);
+        HandGear handGear2 = new HandGear("Glove","Pink",4,6);
+        HandGear handGear3 = new HandGear("Ring","Yellow",7,5);
+        HandGear handGear4 = new HandGear("Fingerless Glove","Blue",3,3);
+
+        FootWear footwear1 = new FootWear("Shoe","Blue",4,4);
+        FootWear footwear2 = new FootWear("Sandal","Purple",1,6);
+        FootWear footwear3 = new FootWear("Flip Flop","Black",3,5);
+        FootWear footwear4 = new FootWear("Roller Skate","Green",7,3);
+
+        //FootWear footwear5 = new FootWear("Roller Skate","Green",7,3);
+
+        //gears list
+        ArrayList<Gear> testGears = new ArrayList<Gear>();
+        testGears.add(headgear1);
+        testGears.add(headgear2);
+        testGears.add(handGear1);
+        testGears.add(handGear2);
+        testGears.add(handGear3);
+        testGears.add(handGear4);
+        testGears.add(footwear1);
+        testGears.add(footwear2);
+        testGears.add(footwear3);
+        testGears.add(footwear3);
+        testGears.add(footwear4);
+
+        Driver driver = new Driver(testFighter,testFighter,testGears);
+
+    }
+
+
 
 }
